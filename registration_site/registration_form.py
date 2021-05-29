c473ae3c2199dbcedd42eb53880a14b504737a9c
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2021-05-29 23:56:13 +0530

    Add few fields

--- registration_site/registration_form.py
+++ registration_site/registration_form.py
@@ -1,32 +1,77 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField, validators, StringField, RadioField
-from wtforms.fields.html5 import DateField, EmailField
+from wtforms import SelectField, validators, StringField, RadioField, TextAreaField, BooleanField, \
+    FloatField, IntegerField as NumberField
+from wtforms.fields.html5 import DateField, EmailField, IntegerField
+
+chapters = ["North Delhi",
+            "South Delhi",
+            "Kurukshetra",
+            "Dehradun",
+            "Kolkata",
+            "Aurangabad",
+            "Phagwara",
+            "Gurugram",
+            "Gr. Noida",
+            "Hydrabad",
+            "Haridwar",
+            "Jaipur",
+            "South Mumbai",
+            "West Mumbai",
+            "Chennai",
+            "Panchkula",
+            "Baddi",
+            "Pune",
+            "Bengaluru",
+            "Vadodara",
+            "Noida",
+            "Ahmedabad"
+            ]
+
+countries = ['India']
+
+states = ['Andhra Pradesh',
+          'Arunachal Pradesh',
+          'Assam',
+          'Bihar',
+          'Chhattisgarh',
+          'Delhi',
+          'Goa',
+          'Gujarat',
+          'Haryana',
+          'Himachal Pradesh',
+          'Jharkhand',
+          'Karnataka',
+          'Kerala',
+          'Madhya Pradesh',
+          'Maharashtra',
+          'Manipur',
+          'Meghalaya',
+          'Mizoram',
+          'Nagaland',
+          'Odisha',
+          'Punjab',
+          'Rajasthan',
+          'Sikkim',
+          'TamilNadu',
+          'Telangana',
+          'Tripura',
+          'Uttar Pradesh',
+          'Uttarakhand',
+          'West Bengal']
+
+union_territories = [
+    'Andaman and Nicobar Islands',
+    'Chandigarh',
+    'Dadra and Nagar Haveli and Daman and Diu',
+    'Jammu and Kashmir',
+    'Ladakh',
+    'Lakshadweep',
+    'Puducherry'
+]
 
 
 class RegistrationForm(FlaskForm):
-    chapter = SelectField('Chapter', [validators.DataRequired()], choices=["North Delhi",
-                                                                           "South Delhi",
-                                                                           "Kurukshetra",
-                                                                           "Dehradun",
-                                                                           "Kolkata",
-                                                                           "Aurangabad",
-                                                                           "Phagwara",
-                                                                           "Gurugram",
-                                                                           "Gr. Noida",
-                                                                           "Hydrabad",
-                                                                           "Haridwar",
-                                                                           "Jaipur",
-                                                                           "South Mumbai",
-                                                                           "West Mumbai",
-                                                                           "Chennai",
-                                                                           "Panchkula",
-                                                                           "Baddi",
-                                                                           "Pune",
-                                                                           "Bengaluru",
-                                                                           "Vadodara",
-                                                                           "Noida",
-                                                                           "Ahmedabad"
-                                                                           ])
+    chapter = SelectField('Chapter', [validators.DataRequired()], choices=chapters)
 
     first_name = StringField('First Name', [validators.DataRequired()])
     middle_name = StringField('Middle Name')
@@ -38,75 +83,124 @@ class RegistrationForm(FlaskForm):
                                  description="Please provide your WhatsApp number if you use "
                                              "WhatsApp")
 
-    home_address = StringField('Home Address', [validators.DataRequired()])
-    country = SelectField('Country', [validators.DataRequired()], choices=['India'])
-    state = StringField('State', [validators.DataRequired()])
+    home_address = TextAreaField('Home address', [validators.DataRequired()],
+                                 description='Please add a landmark')
+    pin_code = NumberField('PIN Code', [validators.DataRequired(), validators.Length(min=6, max=8)])
+
+    country = SelectField('Country', [validators.DataRequired()], choices=countries)
+    state = SelectField('State', [validators.DataRequired()], choices=states)
     city = StringField('City', [validators.DataRequired()])
 
-    email = EmailField('Email Address', [validators.DataRequired()])
-    religion = StringField('Religion', [validators.DataRequired()])
-    caste = StringField('Caste', [validators.DataRequired()])
+    email = EmailField('Email ID', [validators.DataRequired()])
 
-    # field = StringField('S.NO.', [validators.DataRequired()])
-    # field = StringField('First Name', [validators.DataRequired()])
-    # field = StringField('Middle Name', [validators.DataRequired()])
-    # field = StringField('Last Name', [validators.DataRequired()])
-    # field = StringField('Gender', [validators.DataRequired()])
-    # field = StringField('Age', [validators.DataRequired()])
-    # field = StringField('Date of Birth' description='(MM/DD/YYYY)', [validators.DataRequired()])
-    # field = StringField('Contact Number' description='(Whatsapp number if you are using whatsapp application)', [validators.DataRequired()])
-    # field = StringField('Home Address with Pincode' description='( Add landmark)', [validators.DataRequired()])
-    # field = StringField('Country', [validators.DataRequired()])
-    # field = StringField('State', [validators.DataRequired()])
-    # field = StringField('City', [validators.DataRequired()])
-    # field = StringField('Email ID', [validators.DataRequired()])
-    # field = StringField('Religion' description='(for information only)', [validators.DataRequired()])
-    # field = StringField('Caste ', [validators.DataRequired()])
-    # field = StringField('Do you fall under any reservation category of government', [validators.DataRequired()])
-    # field = StringField('Disability' description='(for information only):  Yes / No ', [validators.DataRequired()])
-    # field = StringField('Type of disability if any', [validators.DataRequired()])
-    # field = StringField('Marital status of candidate', [validators.DataRequired()])
-    # field = StringField('Identification Type', [validators.DataRequired()])
-    # field = StringField('Enter your Aadhar Card  Number.', [validators.DataRequired()])
+    religion = StringField('Religion', [validators.DataRequired()],
+                           description='For information only')
+
+    caste = StringField('Caste', [validators.DataRequired()], )
+
+    reservation = RadioField('Do you fall under any reservation category of government',
+                             [validators.DataRequired()],
+                             choices=['General', 'SC', 'ST', 'OBC'])
+
+    has_disability = RadioField('Disability', description='For information only',
+                                choices=['Yes', 'No'])
+    disability = StringField('Type of disability (if any)',
+                             description='If selected \'Yes\' to above question please state')
+
+    marital_status = StringField('Marital status',
+                                 description='Please state if you are single, married or any other')
+
+    identification_type = RadioField('Identification Type', [validators.DataRequired()],
+                                     choices=['Aadhar card'])
+
+    aadhar_card_number = StringField('Aadhar Card Number.',
+                                     [validators.DataRequired(), validators.Length(min=12, max=12)])
+
     # field = StringField('Add your passport size photograph here', [validators.DataRequired()])
-    # field = StringField('Name of the School ', [validators.DataRequired()])
-    # field = StringField('If any other' description='(school name)', [validators.DataRequired()])
-    # field = StringField('Stream' description='(Science /Commerce /Arts/)', [validators.DataRequired()])
-    # field = StringField('If Vocational Course add course details ', [validators.DataRequired()])
-    # field = StringField('Address of School ', [validators.DataRequired()])
-    # field = StringField('Applied Before?', [validators.DataRequired()])
-    # field = StringField('Recently passed 10th/ 12th Std in 2021', [validators.DataRequired()])
-    # field = StringField('Percentage of 10th Board Examination', [validators.DataRequired()])
-    # field = StringField('Distinction in any subject' description='(write above 75 marks obtained subject names)', [validators.DataRequired()])
-    # field = StringField('Passing Year' description='(10th class)', [validators.DataRequired()])
-    # field = StringField('Name of school from where you passed class 10th', [validators.DataRequired()])
-    # field = StringField(' Name of the Board' description='(Class 10th)', [validators.DataRequired()])
-    # field = StringField('Current Class', [validators.DataRequired()])
-    # field = StringField('Excellence in any other Field' description='(Sports, Arts, Debates, etc.)', [validators.DataRequired()])
-    # field = StringField('Hobbies', [validators.DataRequired()])
+
+    school = StringField('School ', [validators.DataRequired()])
+    additional_school = StringField('Any other school')
+
+    stream = RadioField('Stream', [validators.DataRequired()],
+                        choices=['Science', 'Commerce', 'Arts', 'Vocational'])
+
+    vocational_course_details = StringField('Vocational course details',
+                                            description='If you have taken a vocational course,'
+                                                        ' add details here')
+
+    school_address = TextAreaField('Address of School', [validators.DataRequired()])
+
+    applied_before = BooleanField('Applied Before?', [validators.DataRequired()])
+
+    recent_10th_12th = BooleanField('Recently passed 10th/12th Std in 2021?',
+                                    [validators.DataRequired()])
+
+    matric_pct = FloatField('Percentage of 10th Board Examination', [validators.DataRequired()])
+
+    distinction_subjects_10th = StringField('Distinction in any subject',
+                                            description='Mention subjects with 75 marks and above')
+
+    passing_year_10th = IntegerField('Passing Year (10th)', [validators.DataRequired()])
+
+    school_10th = StringField('Name of school from where you passed class 10th',
+                              [validators.DataRequired()])
+    board_10th = StringField(' Name of the Board', [validators.DataRequired()])
+
+    current_class = StringField('Current Class', [validators.DataRequired()])
+
+    excellence_field = StringField('Excellence in any other Field',
+                                   [validators.DataRequired()],
+                                   description='(Sports, Arts, Debates, etc.)')
+
+    hobbies = StringField('Hobbies', [validators.DataRequired()])
+
     # field = StringField('Add copy of your 10th Marksheet', [validators.DataRequired()])
     # field = StringField('Add fee slip if any of class 11th', [validators.DataRequired()])
-    # field = StringField('Father's Name', [validators.DataRequired()])
-    # field = StringField('Father's Age ', [validators.DataRequired()])
-    # field = StringField('Father's Gender', [validators.DataRequired()])
-    # field = StringField('Father's Qualification ', [validators.DataRequired()])
-    # field = StringField('Marital status of Father', [validators.DataRequired()])
-    # field = StringField('Occupation' description='(What work your father does)', [validators.DataRequired()])
-    # field = StringField('Father's monthly income' description='(Salary)', [validators.DataRequired()])
-    # field = StringField('Mother's Name ', [validators.DataRequired()])
-    # field = StringField('Mother's Age ', [validators.DataRequired()])
-    # field = StringField('Mother's Gender', [validators.DataRequired()])
-    # field = StringField('Mother's Qualification ', [validators.DataRequired()])
-    # field = StringField('Mother's Marital Status ', [validators.DataRequired()])
-    # field = StringField('Occupation' description='(What work your mother does)', [validators.DataRequired()])
-    # field = StringField('Mother monthly income' description='(Salary)', [validators.DataRequired()])
-    # field = StringField('Single Parents/No Parents', [validators.DataRequired()])
-    # field = StringField('If case of Single parent, Who is the parent?', [validators.DataRequired()])
-    # field = StringField('Details about Guardian  ' description='(Details like age, education, married or not, working or not)', [validators.DataRequired()])
-    # field = StringField('Do you have brother /sister?', [validators.DataRequired()])
-    # field = StringField('Details about Brothers & Sisters' description='(if you have any)' description='(Details like age, education, married or not, working or not)', [validators.DataRequired()])
-    # field = StringField('Annual Family Income', [validators.DataRequired()])
-    # field = StringField('Is any of your sibling has been a beneficiary of the Udayan Shalini Fellowships' description='(If yes, give details)', [validators.DataRequired()])
+
+    fathers_name = StringField("Father's Name", [validators.DataRequired()])
+    fathers_age = IntegerField("Father's Age", [validators.DataRequired()])
+
+    fathers_gender = StringField("Father's Gender", [validators.DataRequired()])
+    fathers_qualification = StringField("Father's Qualification", [validators.DataRequired()])
+
+    fathers_marital_status = StringField('Marital status of Father', [validators.DataRequired()])
+
+    fathers_occupation = StringField('Occupation', [validators.DataRequired()],
+                                     description='What work your father does')
+    fathers_monthly_income = IntegerField("Father's monthly income", [validators.DataRequired()],
+                                          description="Salary")
+
+    mothers_name = StringField("Mother's Name", [validators.DataRequired()])
+    mothers_age = IntegerField("Mother's Age", [validators.DataRequired()])
+
+    mothers_gender = StringField("Mother's Gender", [validators.DataRequired()])
+    mothers_qualification = StringField("Mother's Qualification", [validators.DataRequired()])
+
+    mothers_marital_status = StringField('Marital status of Mother', [validators.DataRequired()])
+
+    mothers_occupation = StringField('Occupation', [validators.DataRequired()],
+                                     description='What work your mother does')
+    mothers_monthly_income = IntegerField("Mother's monthly income", [validators.DataRequired()],
+                                          description="Salary")
+
+    has_single_parents = BooleanField('Single Parents/No Parents')
+    who_is_single_parent = RadioField('If case of Single parent, Who is the parent?',
+                                      choices=['Mother', 'Father'])
+
+    guardian_details = StringField('Details about Guardian',
+                                   description='Details like age, education, married?, occupation')
+
+    has_sibling = BooleanField('Do you have brother/sister?', [validators.DataRequired()])
+
+    sibling_details = TextAreaField('Details about Brothers & Sisters (if any)',
+                                    description='Details like age, education, married or not,'
+                                                ' working or not')
+
+    annual_family_income = StringField('Annual Family Income', [validators.DataRequired()])
+    sibling_beneficiary = StringField(
+        'Is any of your sibling has been a beneficiary of the Udayan Shalini Fellowships?',
+        description='(If yes, give details)')
+
     # field = StringField('Assets owned by the family' description='(Give Details)A] Immovable property. ', [validators.DataRequired()])
     # field = StringField('Details, if any other', [validators.DataRequired()])
     # field = StringField('Attach images of your Address proof.' description='(attach Ration Card Or  Aadhar Card of father or mother)', [validators.DataRequired()])
@@ -127,10 +221,3 @@ class RegistrationForm(FlaskForm):
     # field = StringField('Attach  Candidate Proof of Bank Account' description='(Copy of passbook)', [validators.DataRequired()])
     # field = StringField('Attach a photo of Aadhaar Card of the applicant', [validators.DataRequired()])
     # field = StringField('DECLARATION1. I, hereby, declare that all the information furnished above is true to the best of my knowledge. I also understand that if any of the above information is found to be incorrect, the fellowship will be withdrawn 2. I have read all the rules and regulations of the organization and promise to follow them. I also understand that in case my fellowship is withdrawn due to any of the reasons mentioned in Annexure1, the decision of the organization will be final and abiding', [validators.DataRequired()])
-
-    # password = PasswordField('New Password', [
-    #     validators.DataRequired(),
-    #     validators.EqualTo('confirm', message='Passwords must match')
-    # ])
-    # confirm = PasswordField('Repeat Password')
-    # accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
